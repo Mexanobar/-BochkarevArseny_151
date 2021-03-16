9cd338382c8b43a40046325d725c7c0dfb3efb31
Mexanobar <mexanobar@gmail.com>
2021-03-16 18:25:15 +0300

    Howmework 2

---
+++ Home task/Semester 2/16.02.21/prog1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long fact(long long n)
+{
+	return (n > 1 ? n*fact(n-1) : 1);
+}
+
+double generateSin(double x)
+{ 
+	long double res = 0;
+
+	for (long long i = 0; i < 15; i++) 
+
+
+// в силу переполнения типа long long рассчёт пойдёт до n = 14
+	{
+		res += ((pow(-1, i)) * pow(x, 2*i + 1)) / double(fact(2*i + 1));
+	}
+
+	return res;
+}
+
+
+int main()
+{
+	double x, new_sin;
+
+	cin >> x;
+
+	//cout.setf(ios::scientific);
+	cout.precision(10);
+
+	new_sin = generateSin(x);
+
+	cout << new_sin << "\n";
+
+	//cout << sin(x);
+
+	return 0;
+}
